566544f83b73eed89954d8cf2ea98f9cda9adc43
Ryan <rcosby@esri.com>
2023-04-24 13:59:37 -0700

    modification to helpers to adjust to new 3.0 SDK

---
+++ 3.0/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Threading.Tasks;
+
+namespace DataAssistant
+{
+  internal static class Helpers
+  {
+    internal static FeatureLayer CreateFeatureLayer(Uri uri, Map map)
+        {
+      QueuedTask.Run(() =>
+      {
+       var createParams = new FeatureLayerCreationParams(uri);
+       return LayerFactory.Instance.CreateLayer<FeatureLayer>(createParams, MapView.Active.Map);
+      }
+      );
+      return null;
+    }
+    
+    internal static Task AddProjectItem(string path)
+    {
+      var folderToAdd = ItemFactory.Instance.Create(path);
+      return QueuedTask.Run(() => Project.Current.AddItem(folderToAdd as IProjectItem));
+    } 
+  }
+}
